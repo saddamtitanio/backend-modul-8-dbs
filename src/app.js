118500764b6afc09d3363b1ea49d9f1de8fe636c
saddamtitanio <saddam.titanio25@gmail.com>
2026-05-04 21:00:58 +0700

    changes

--- src/app.js
+++ src/app.js
@@ -20,7 +20,8 @@ const allowedOrigins = [
   'https://frontendmodule10.vercel.app',
   'https://game-vault-ten.vercel.app',
   'https://gear-vault-rouge.vercel.app',
-  'https://dbscs-10.vercel.app'
+  'https://dbscs-10.vercel.app',
+  'https://cs-es-hanif-mulia-nugroho-240636906.vercel.app'
 ];
 
 const corsOptions = {
